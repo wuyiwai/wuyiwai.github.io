d95dec2f801767269f9a2236a0792ab1533fcda0
wuyiwai <wuyiwai@xiaoman.cn>
2025-04-25 14:51:32 +0800

    fix: update deploy yml

--- astro.config.ts
+++ astro.config.ts
@@ -33,8 +33,9 @@ export default defineConfig({
     experimentalLayout: "responsive",
   },
   experimental: {
-    svg: true,
-    responsiveImages: true,
-    preserveScriptOrder: true,
+    // 检查以下实验特性的拼写和版本是否正确
+    // svg: true,
+    // responsiveImages: true,
+    // preserveScriptOrder: true,
   },
 });
